69e5fb2c98f729ef142522f9593802d40b196a73
CodeWizard-1 Oleksandr Kravchenko <kravchenko.dp@gmail.com>
2023-08-28 14:21:47 +0000

    Fixed name id in html and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,9 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
+
     runGame("addition");
+
 });
 
 /**
@@ -33,6 +35,7 @@ function runGame(gameType) {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
+
 }
 
 /**
@@ -40,6 +43,7 @@ function runGame(gameType) {
  * the returned calculateCorrectAnswer array
  */
 function checkAnswer() {
+
     let userAnswer = parseInt(document.getElementById("answer-box").value);
     let calculatedAnswer = calculateCorrectAnswer();
     let isCorrect = userAnswer === calculatedAnswer[0];
@@ -47,18 +51,31 @@ function checkAnswer() {
     if (isCorrect) {
         alert("Hey! You got it right! :D");
     } else {
-        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);   
+        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`); 
     }
 
     runGame(calculatedAnswer[1]);
 
 }
 
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and return the correct answer.
+ * 
+ */
 function calculateCorrectAnswer() {
 
     let operand1 = parseInt(document.getElementById('operand1').innerText);
-    let operand2 = parseInt(document.getElementById('operand2').innertext);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
     let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return[operand1 + operand2, "additional"];
+    } else {
+        alert(`Uniplemented operator ${operator}`);
+        throw`Uniplemented operator ${operator}. Aborting!`;
+
+    }
 
 }
 
